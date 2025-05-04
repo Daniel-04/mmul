dee3bdcd9d22412fb81e5f27589dffce607a3c6e
Daniel <danielelacer08@gmail.com>
2025-05-04 19:14:53 +0100

    order of operations/cache usage fix

--- mmul.c
+++ mmul.c
@@ -87,14 +87,10 @@ void tiled_transposed_mmul(int M1, int N2, int N1, const float *restrict A,
     if (!_B)
         return;
 
-    // Initialise C to zero
-    for (int i = 0; i < M1; i++) {
-        for (int j = 0; j < N2; j++) {
-            C[i * N2 + j] = 0;
-        }
+    for (int i = 0; i < M1 * N2; i++) {
+        C[i] = 0;
     }
 
-    // Transpose B into _B
     for (int i = 0; i < N1; i++) {
         for (int j = 0; j < N2; j++) {
             _B[j * N1 + i] = B[i * N2 + j];
@@ -127,23 +123,19 @@ void openmp_tiled_transposed_mmul(int M1, int N2, int N1,
     if (!_B)
         return;
 
-    // Initialise C to zero
-#pragma omp parallel for collapse(2)
-    for (int i = 0; i < M1; i++) {
-        for (int j = 0; j < N2; j++) {
-            C[i * N2 + j] = 0;
-        }
+#pragma omp parallel for
+    for (int i = 0; i < M1 * N2; i++) {
+        C[i] = 0;
     }
 
-    // Transpose B into _B
-#pragma omp parallel for collapse(2)
+#pragma omp parallel for
     for (int i = 0; i < N1; i++) {
         for (int j = 0; j < N2; j++) {
             _B[j * N1 + i] = B[i * N2 + j];
         }
     }
 
-#pragma omp parallel for collapse(2)
+#pragma omp parallel for
     for (int it = 0; it < M1; it += TILE_SIZE) {
         for (int jt = 0; jt < N2; jt += TILE_SIZE) {
             for (int kt = 0; kt < N1; kt += TILE_SIZE) {
